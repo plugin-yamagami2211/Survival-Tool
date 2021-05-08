1bbac3b7bece084131cf9491db47c932103e813c
yamagami2211 <mail@yama2211.jp>
2021-05-08 16:25:38 +0900

    初めてのログインとログアウトを実装

--- src/main/java/jp/yama2211/st/EventListener.java
+++ src/main/java/jp/yama2211/st/EventListener.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventListener implements Listener {
     public Main plugin;
@@ -20,7 +21,22 @@ public class EventListener implements Listener {
             Join = Join.replaceAll("%n","\n");
             event.setJoinMessage(null);
             Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',Join));
+        } else {
+            String FJoin = plugin.getConfig().getString("Msg.FJoin");
+            FJoin =FJoin.replace("%player",event.getPlayer().getName());
+            FJoin = FJoin.replaceAll("%n","\n");
+            event.setJoinMessage(null);
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',FJoin));
         }
+    }
+
+    @EventHandler
+    public void onLogout(PlayerQuitEvent event){
+        String Quit = plugin.getConfig().getString("Msg.Quit");
+        Quit =Quit.replace("%player",event.getPlayer().getName());
+        Quit = Quit.replaceAll("%n","\n");
+        event.setQuitMessage(null);
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',Quit));
     }
 
 }
