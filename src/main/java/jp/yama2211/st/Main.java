49a9b27589e302a0e5d90492a37be0ad979d111d
yamagami2211 <yamagami2211@svk.jp>
2021-05-08 12:48:32 +0900

    初

---
+++ src/main/java/jp/yama2211/st/Main.java
@@ -0,0 +1,17 @@
+package jp.yama2211.st;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
