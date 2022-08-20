cc06d8d45a9ad327b5de7f1de479c178f9f5ae42
yamagami2211 <mail@yama2211.jp>
2022-08-20 18:00:18 +0900

    winfoの項目を増やした。スポーンロケーションをxyzから取得するように変更。

--- src/main/java/jp/yama2211/st/Cmd/WorldInfoCmd.java
+++ src/main/java/jp/yama2211/st/Cmd/WorldInfoCmd.java
@@ -22,19 +22,34 @@ public class WorldInfoCmd implements CommandExecutor {
             try{
                 World w = Bukkit.getWorld(args[0]);
                 String wName = w.getName(); //WorldName
-                String wSL = w.getSpawnLocation().toString(); //SpwanLocation(toString)
+                String wSLx = Integer.toString(w.getSpawnLocation().getBlockX());
+                String wSLy = Integer.toString(w.getSpawnLocation().getBlockY());
+                String wSLz = Integer.toString(w.getSpawnLocation().getBlockZ());
                 Boolean wAllowAnimal = w.getAllowAnimals();
                 Boolean wAllowMonsters = w.getAllowMonsters();
                 Boolean wPvP = w.getPVP();
                 String wDifficulty = w.getDifficulty().toString();
+                String wAmbient = Integer.toString(w.getAmbientSpawnLimit());
+                String wAnimal = Integer.toString(w.getAnimalSpawnLimit());
+                String wMonster = Integer.toString(w.getMonsterSpawnLimit());
+                String wMaxHeight = Integer.toString(w.getMaxHeight());
+                String wMinHeight = Integer.toString(w.getMinHeight());
 
                 String wMsg = plugin.getConfig().getString("WInfo.msg");
-                wMsg = wMsg.replace("%Name",wName);
-                wMsg = wMsg.replace("%SL",wSL);
-                wMsg = wMsg.replace("%AllowAnimal",wAllowAnimal.toString());
-                wMsg = wMsg.replace("%AllowMonsters",wAllowMonsters.toString());
-                wMsg = wMsg.replace("%PvP",wPvP.toString());
-                wMsg = wMsg.replace("%Difficulty",wDifficulty);
+                wMsg = wMsg.replace("%Name",wName); //WorldName
+                wMsg = wMsg.replace("%SLx",wSLx); //SpawnLocation x
+                wMsg = wMsg.replace("%SLy",wSLy); //SpawnLocation y
+                wMsg = wMsg.replace("%SLz",wSLz); //SpawnLocation z
+                wMsg = wMsg.replace("%AllowAnimal",wAllowAnimal.toString()); //Animal Spawn Boolean
+                wMsg = wMsg.replace("%AllowMonsters",wAllowMonsters.toString()); //Monster Spawn Boolean
+                wMsg = wMsg.replace("%PvP",wPvP.toString()); //PvPBoolean
+                wMsg = wMsg.replace("%Difficulty",wDifficulty); //Diffculty
+                wMsg = wMsg.replace("%Ambient",wAmbient); //Ambient Spawn Limit
+                wMsg = wMsg.replace("%Animal",wAnimal); //Animal Spawn Limit
+                wMsg = wMsg.replace("%Monster",wMonster); //Monster Spawn Limit
+                wMsg = wMsg.replace("%MaxHeight",wMaxHeight); //World Y Max
+                wMsg = wMsg.replace("%MinHeight",wMinHeight); //World Y Min
+
                 wMsg = wMsg.replaceAll("%n","\n");
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&',wMsg));
             }catch (Exception e){
